02a0d9f60f67f7732935d5a61909964911466cb9
Alex Cline <rev.a.r.cline@gmail.com>
2021-01-20 13:19:53 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,12 @@
 require_relative 'config/environment'
 
+# it's an app babey
 class App < Sinatra::Base
+  get '/' do
+    erb :index
+  end
 
-	get '/' do
-		
-	end
+  get '/info' do
+    erb :info
+  end
 end
